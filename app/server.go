44a589bed3ad778b3d552857082698483bec7bfc
kjabin <185402721+kjabin@users.noreply.github.com>
2024-12-06 10:27:26 +0530

    add http response header

--- app/server.go
+++ app/server.go
@@ -13,9 +13,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
