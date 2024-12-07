64b947f7efd3e8e48b6bca12e7d3eecfd8c1ab1f
kjabin <185402721+kjabin@users.noreply.github.com>
2024-12-07 17:54:12 +0530

    add http header support

--- http/http.go
+++ http/http.go
@@ -1,9 +1,10 @@
 package http
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"strconv"
 	"strings"
 )
 
@@ -28,15 +29,69 @@ func (s *Server) ListenAndServe() error {
 
 func handleConn(conn net.Conn) error {
 	defer conn.Close()
-	req, err := bufio.NewReader(conn).ReadString('\n')
+	req, err := parseRequest(conn)
 	if err != nil {
 		return err
 	}
-	headers := strings.Fields(req)
-	if headers[1] == "/" {
+
+	// connection handlers
+	if req.Method == "GET" && req.Path == "/" {
 		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+	} else if req.Method == "GET" && strings.HasPrefix(req.Path, "/echo") {
+
+		query := strings.SplitN(req.Path[1:], "/", 2)[1]
+		header := make(Header)
+		header.Add("Content-Type", "text/plain")
+		header.Add("Content-Length", strconv.Itoa(len(query)))
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n%v\r\n%v", header, query)
 	} else {
 		fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 	}
 	return nil
+}
+
+type Header map[string][]string
+
+func (h Header) Add(key, value string) {
+	h[key] = append(h[key], value)
+}
+
+func (h Header) String() string {
+	s := ""
+	for k, values := range h {
+		for _, v := range values {
+			s += fmt.Sprintf("%v: %v\r\n", k, v)
+		}
+	}
+	return s
+}
+
+type Request struct {
+	Method string
+	Path   string
+	Header Header
+	Body   io.Reader
+}
+
+func parseRequest(conn net.Conn) (Request, error) {
+	buf := make([]byte, 2048)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return Request{}, err
+	}
+	sections := strings.Split(string(buf[:n]), "\r\n")
+	status := strings.Split(sections[0], " ")
+
+	req := Request{
+		Method: status[0],
+		Path:   status[1],
+		Header: make(Header),
+	}
+
+	for _, header := range sections[1 : len(sections)-2] {
+		items := strings.Split(header, ": ")
+		req.Header.Add(items[0], items[1])
+	}
+	req.Body = strings.NewReader(sections[len(sections)-1])
+	return req, nil
 }
