242c26718a517b3ff91a3ea228e4213d3e4ab70a
kjabin <185402721+kjabin@users.noreply.github.com>
2024-12-08 21:39:02 +0530

    add new http status codes

--- http/status.go
+++ http/status.go
@@ -1,19 +1,28 @@
 package http
 
 const (
-	StatusOK         int = 200
-	StatusNotFound   int = 404
-	StatusNotAllowed int = 405
+	StatusOK                  int = 200
+	StatusCreated             int = 201
+	StatusNotFound            int = 404
+	StatusNotAllowed          int = 405
+	StatusBadRequest          int = 400
+	StatusInternalServerError int = 501
 )
 
 func StatusText(code int) string {
 	switch code {
 	case StatusOK:
 		return "OK"
+	case StatusCreated:
+		return "Created"
 	case StatusNotFound:
 		return "Not Found"
 	case StatusNotAllowed:
 		return "Method Not Allowed"
+	case StatusBadRequest:
+		return "Bad Request"
+	case StatusInternalServerError:
+		return "Internal Server Error"
 	default:
 		return ""
 	}
