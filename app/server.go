55a4a6ca59a909860748d6b635c0c950875032dc
kjabin <185402721+kjabin@users.noreply.github.com>
2024-12-08 20:20:34 +0530

    add http file handler

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"strconv"
 	"strings"
 
@@ -17,18 +18,36 @@ func main() {
 	mux.HandleFunc("GET /echo", handleEcho)
 	mux.HandleFunc("GET /user-agent", handleUserAgent)
 	mux.HandleFunc("GET /", handleIndex)
+	mux.HandleFunc("GET ", handleDefault)
+	mux.HandleFunc("GET /files", handleFiles)
 	log.Fatal(s.ListenAndServe())
 }
 
+func handleFiles(w *http.ResponseWriter, r *http.Request) {
+	filename := strings.SplitN(r.Path[1:], "/", 2)[1]
+	if _, err := os.Stat(filename); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Add("Content-Type", "application/octet-stream")
+	w.Header().Add("Content-Length", strconv.Itoa(len(data)))
+	w.Write(data)
+}
+
 func handleIndex(w *http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 }
 
 func handleEcho(w *http.ResponseWriter, r *http.Request) {
 	query := strings.SplitN(r.Path[1:], "/", 2)[1]
 	w.Header().Add("Content-Length", strconv.Itoa(len(query)))
 	w.Header().Add("Content-Type", "text/plain")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(query))
 }
 
@@ -36,10 +55,10 @@ func handleUserAgent(w *http.ResponseWriter, r *http.Request) {
 	userAgent := r.Header.Get("User-Agent")
 	w.Header().Add("Content-Length", strconv.Itoa(len(userAgent)))
 	w.Header().Add("Content-Type", "text/plain")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(userAgent))
 }
 
 func handleDefault(w *http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(404)
+	w.WriteHeader(http.StatusNotFound)
 }
